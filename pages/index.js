a026ca5f347acfa8c4c72a150569603def79bdc5
Jeong-Ah Hong <jahong89@gmail.com>
2020-07-23 15:48:04 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Animation from '../components/Animation';
+import Animation2 from '../components/Animation2';
+
+const Home = () => {
+
+  return (
+    <>
+      <Animation/>
+      <Animation2 />
+    </>
+  );
+}
+
+export default Home;
